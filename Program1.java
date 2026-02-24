cb15ff14e74d96c1e8c1d475fc7a76574a66bfad
Amit Ahirwar <ahirwaramit817@gmail.com>
2026-02-24 21:55:36 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,10 @@
+public class Program1 {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        int num1 = 23;
+        int num2 = 234;
+        int num3 = 453;
+        int sum = num1 + num2 + num3;
+        System.out.println("the sum of these no. is :" + sum);
+    }
+}
